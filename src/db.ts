a8b4c42d15c171ad24d0079ef97d843d9e633094
Nathan <nathanmargaglio@gmail.com>
2018-10-29 13:30:08 -0400

    add back using env variable

--- src/db.ts
+++ src/db.ts
@@ -4,31 +4,38 @@ export class DB {
 		public sequelize: Sequelize;
 
 		constructor(test=false) {
-        if (test){
-            this.sequelize = this.testSequelizer();
-        } else {
-            this.sequelize = this.sequelizer();
-        }
+				if (test){
+						this.sequelize = this.testSequelizer();
+				} else {
+						this.sequelize = this.sequelizer();
+				}
 		}
 
 		private sequelizer(): Sequelize {
+				if (process.env.DATABASE_URL) {
+						return new Sequelize({
+								url: process.env.DATABASE_URL,
+								modelPaths: [__dirname + '/models']								
+						});		
+				} else {
+						return new Sequelize({
+								database: 'api_dev',
+								dialect: 'postgres',
+								username: 'postgres',
+								password: null,
+								port: 15432,
+								modelPaths: [__dirname + '/models']
+						});
+				}
+		}
+
+		private testSequelizer(): Sequelize {
 				return new Sequelize({
-						database: 'api_dev',
+						database: 'api_test',
 						dialect: 'postgres',
 						username: 'postgres',
 						password: null,
 						port: 15432,
-						modelPaths: [__dirname + '/models']
-				});
-		}
-
-    private testSequelizer(): Sequelize {
-        return new Sequelize({
-						database: 'api_test',
-						dialect: 'postgres',
-						username: 'postgres',
-            password: null,
-            port: 15432,
 						modelPaths: [__dirname + '/models']
 				});
 		}
